d02949a64d848934ba68785a9ff82175afc01551
RYO UEOKA <koara@KOARAs-MBP.local>
2017-01-12 02:03:47 +0900

    [ADD]sidebar prototype

--- js/index.js
+++ js/index.js
@@ -46,3 +46,76 @@ $(function() {
     });
 });
 /*   isotope end   */
+
+( function ( $ ) {
+ // Initialize Slidebars
+ var controller = new slidebars();
+ controller.init();
+
+ /**
+ * Original Version Style Control Classes
+ */
+
+ // Open left
+ $( '.sb-open-left' ).on( 'click', function ( event ) {
+ event.preventDefault();
+ event.stopPropagation();
+
+ controller.open( 'sb-left' );
+ } );
+
+ // Toggle left
+ $( '.sb-toggle-left' ).on( 'click', function ( event ) {
+ event.preventDefault();
+ event.stopPropagation();
+
+ controller.toggle( 'sb-left' );
+ } );
+
+ // Open right
+ $( '.sb-open-right' ).on( 'click', function ( event ) {
+ event.preventDefault();
+ event.stopPropagation();
+
+ controller.open( 'sb-right' );
+ } );
+
+ // Toggle right
+ $( '.sb-toggle-right' ).on( 'click', function ( event ) {
+ event.preventDefault();
+ event.stopPropagation();
+
+ controller.toggle( 'sb-right' );
+ } );
+
+ // Close
+ $( '.sb-close' ).on( 'click', function ( event ) {
+ event.preventDefault();
+ event.stopPropagation();
+
+ controller.close();
+ } );
+} ) ( jQuery );
+
+
+$(function(){
+ 
+    // モーダルウィンドウが開くときの処理    
+    $(".modalOpen").click(function(){
+            
+        var navClass = $(this).attr("class"),
+            href = $(this).attr("href");
+                
+            $(href).fadeIn();
+        $(this).addClass("open");
+        return false;
+    });
+     
+    // モーダルウィンドウが閉じるときの処理    
+    $(".modalClose").click(function(){
+        $(this).parents(".modal").fadeOut();
+        $(".modalOpen").removeClass("open");
+        return false;
+    });  
+    
+});
